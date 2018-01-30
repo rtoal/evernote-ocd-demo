a1ad2a8d5d2609ba2209f13c70db64a133406814
Ray Toal <rtoal@lmu.edu>
2018-01-30 00:52:03 -0800

    Add nodemon and body-parser

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('views'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
 app.post('/recognize', (req, res) => {
-  console.log('Got something');
+  console.log(`Got ${req.body.secret}`);
   res.redirect(path.join('report.html'))
 });
 
