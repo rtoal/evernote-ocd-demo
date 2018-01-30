73068ae6bdb14536203541a88181a141a5974fad
Ray Toal <rtoal@lmu.edu>
2018-01-30 01:13:50 -0800

    Use Pug

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(express.static('views'));
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug')
+app.set('views', './views')
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.render('index');
 });
 
 app.post('/recognize', (req, res) => {
   console.log(`Got ${req.body.secret}`);
-  res.redirect(path.join('report.html'))
+  res.redirect('/report')
+});
+
+app.get('/report', (req, res) => {
+  res.render('report', {name: 'XSS test <script>alert(1)</script>'});
 });
 
 app.listen(3000, () => console.log('Demo running on port 3000...'));
